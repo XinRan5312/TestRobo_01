68ddf48bc38daa0ca7e7d07896337ce441ec45c0
wangkangfei <wangakangfei@baijiahulian.com>
2016-03-31 17:04:04 +0800

    add standard shadow

--- app/src/main/java/com/boomstack/testrobo_01/MainActivity.java
+++ app/src/main/java/com/boomstack/testrobo_01/MainActivity.java
@@ -1,7 +1,8 @@
 package com.boomstack.testrobo_01;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.tv);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setClass(MainActivity.this, OtherActivity.class);
+                startActivity(i);
+            }
+        });
         btn = (Button) findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/test/java/com/boomstack/testrobo_01/MainActivityTest.java
+++ app/src/test/java/com/boomstack/testrobo_01/MainActivityTest.java
@@ -1,5 +1,7 @@
 package com.boomstack.testrobo_01;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,7 +12,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.util.ActivityController;
 
 /**
  * Created by bjhl on 16/3/31.
@@ -43,5 +48,28 @@ public class MainActivityTest {
         btn.performClick();
         Assert.assertEquals("Hola", tv.getText().toString());
     }
+
+    @Test
+    public void testLifecycle() {
+        ActivityController controller = Robolectric.buildActivity(MainActivity.class).create().start();
+        Activity activity = (Activity) controller.get();
+        Assert.assertNotNull(activity);
+        controller.resume();
+        Assert.assertEquals("Hello World!", tv.getText().toString());
+        btn.performClick();
+        Assert.assertEquals("Hola", tv.getText().toString());
+    }
+
+    @Test
+    public void testIntent() {
+        tv.performClick();
+        Intent exceptedIntent = new Intent(mainActivity, OtherActivity.class);
+        //real activity
+        ShadowActivity shadowActivity = Shadows.shadowOf(mainActivity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        Assert.assertEquals(exceptedIntent, actualIntent);
+
+    }
+
 
 }
